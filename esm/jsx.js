780e2159dac4c20234753e3119cc5d7892c99e70
vsmid <vedransmid@gmail.com>
2023-07-04 21:45:31 +0200

    Function hhjsx no longer passes 'tag' parameter in case tag is of type function

--- esm/jsx.js
+++ esm/jsx.js
@@ -2,7 +2,7 @@ import { define } from './html.js';
 
 export function hhjsx(tag, props, ...children) {
   if (typeof tag === 'function') {
-    return tag(tag ?? '', props, children);
+    return tag(props ?? {}, children);
   } else {
     return define(tag)(props ?? {}, children);
   }
